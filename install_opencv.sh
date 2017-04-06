ea13a74dfd4c4b424ea858f8b8149ac4695f7ac1
mhaghighat <mba.haghighat@gmail.com>
2017-04-06 16:49:49 -0700

    Updated the dependencies

--- install_opencv.sh
+++ install_opencv.sh
@@ -4,12 +4,17 @@ sudo apt-get update
 sudo apt-get -y upgrade
 echo "Removing any pre-installed ffmpeg and x264"
 sudo apt-get -qq remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
-sudo apt-get -y install build-essential cmake pkg-config libjpeg62-dev libjasper-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libdc1394-22-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev python-dev python-numpy libtbb-dev libqt4-dev x264 v4l-utils git
+echo "Installing the compiler"
+sudo apt-get -y install build-essential
+echo "Installing required dependenices"
+sudo apt-get -y install cmake git pkg-config libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev
+echo "Installing optional dependenices"
+sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get -y install libv4l-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libqt4-dev x264 v4l-utils
 
-mkdir ~/OpenCV
-cp compile_opencv.sh ~/OpenCV/
-cd ~/OpenCV
+mkdir ~/opencv
+cp compile_opencv.sh ~/opencv/
+cd ~/opencv
 echo "Downloading OpenCV"
 git clone https://github.com/opencv/opencv.git
 echo "Downloading OpenCV's extra modules"
@@ -20,7 +25,7 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ../opencv
 cmake -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
-make -j5
+make -j7
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
